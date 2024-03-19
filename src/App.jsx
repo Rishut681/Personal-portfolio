79f7ea3cd7e189374551685f5d4a0626c9b46b89
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2024-03-19 20:45:11 +0530

    Nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './App.module.css';
-import { About } from "./components/About/About";
-import { Contact } from "./components/Contact/Contact";
-import { Hero } from "./components/Hero/Hero";
+import styles from'./App.module.css';
+// import { About } from "./components/About/About";
+// import { Contact } from "./components/Contact/Contact";
+// import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Projects } from "./components/Projects/Projects";
+// import { Projects } from "./components/Projects/Projects";
 
 function App() {
 
   return (
     <div className={styles.App}>
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <About />
       <Projects />
-      <Contact />
+      <Contact /> */}
     </div>
   )
 }
